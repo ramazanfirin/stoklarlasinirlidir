52bbb91afc767923b1d528de8aa3024f49951360
ramazanfirin@gmail.com <ramazanfirin@gmail.com@3e63ede2-dacc-7fd9-6485-31b5a1e49b6f>
2011-08-20 20:06:00 +0000

    DB ayarları yapıldı.

---
+++ trunk/opencart149/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1:85/opencart149/admin/');
+define('HTTP_CATALOG', 'http://127.0.0.1:85/opencart149/');
+define('HTTP_IMAGE', 'http://127.0.0.1:85/opencart149/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1:85/opencart149/admin/');
+define('HTTPS_IMAGE', 'http://127.0.0.1:85/opencart149/image/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\opencart149/admin/');
+define('DIR_SYSTEM', 'C:\wamp\www\opencart149/system/');
+define('DIR_DATABASE', 'C:\wamp\www\opencart149/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\opencart149/admin/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\opencart149/admin/view/template/');
+define('DIR_CONFIG', 'C:\wamp\www\opencart149/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\opencart149/image/');
+define('DIR_CACHE', 'C:\wamp\www\opencart149/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\opencart149/download/');
+define('DIR_LOGS', 'C:\wamp\www\opencart149/system/logs/');
+define('DIR_CATALOG', 'C:\wamp\www\opencart149/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', '127.0.0.1:3306');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'opencart149');
+define('DB_PREFIX', '');
+?>

b2b01b7bf7df461dc094b06214b694ccb34b4c04
ramazanfirin@gmail.com <ramazanfirin@gmail.com@3e63ede2-dacc-7fd9-6485-31b5a1e49b6f>
2011-08-20 20:06:00 +0000

    DB ayarları yapıldı.

---
+++ opencart149/config.php
@@ -0,0 +1,21 @@
+<?php
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\opencart149/catalog/');
+define('DIR_SYSTEM', 'C:\wamp\www\opencart149/system/');
+define('DIR_DATABASE', 'C:\wamp\www\opencart149/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\opencart149/catalog/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\opencart149/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\wamp\www\opencart149/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\opencart149/image/');
+define('DIR_CACHE', 'C:\wamp\www\opencart149/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\opencart149/download/');
+define('DIR_LOGS', 'C:\wamp\www\opencart149/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', '127.0.0.1:3306');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'opencart149');
+define('DB_PREFIX', '');
+?>

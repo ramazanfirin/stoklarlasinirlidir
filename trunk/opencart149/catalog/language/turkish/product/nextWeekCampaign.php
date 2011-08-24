27fe6f5cf19a6ace5e599a87e153bfaf08a94748
ramazanfirin@gmail.com <ramazanfirin@gmail.com@3e63ede2-dacc-7fd9-6485-31b5a1e49b6f>
2011-08-24 19:15:15 +0000

    nextWeekCampaign sayfası eklendi.

---
+++ trunk/opencart149/catalog/language/turkish/product/nextWeekCampaign.php
@@ -0,0 +1,20 @@
+<?php
+/* OpenCart 1.4.9.5 Turkish Ceviren eka7a - http://www.opencart-tr.com */
+
+// Heading
+$_['heading_title']     = 'Kampanyalı Ürünler';
+
+// Text
+$_['text_empty']        = 'Şuan kampanyalı ürün bulunmamaktadır.';
+$_['text_stars']        = '%s 5 Yıldızlı!';
+$_['text_sort']         = 'Sırala:';
+$_['text_default']      = 'Varsayılan';
+$_['text_name_asc']     = 'A\'dan - Z\'ye';
+$_['text_name_desc']    = 'Z\'den - A\'ya';
+$_['text_price_asc']    = 'Ucuzdan &gt; Pahalıya';
+$_['text_price_desc']   = 'Pahalıdan &gt; Ucuza';
+$_['text_rating_asc']   = 'Düşük Oylama'; 
+$_['text_rating_desc']  = 'Yüksek Oylama';
+$_['text_model_asc']    = 'Ürün Koduna Göre A - Z';
+$_['text_model_desc']   = 'Ürün Koduna Göre Z - A';
+?>

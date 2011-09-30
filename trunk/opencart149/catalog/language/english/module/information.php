3f300be464c4e2c14a53db86e82f4326edca640f
ramazanfirin@gmail.com <ramazanfirin@gmail.com@3e63ede2-dacc-7fd9-6485-31b5a1e49b6f>
2011-09-30 21:17:30 +0000

    customer support

--- trunk/opencart149/catalog/language/english/module/information.php
+++ trunk/opencart149/catalog/language/english/module/information.php
@@ -5,4 +5,5 @@ $_['heading_title'] = 'Information';
 // Text
 $_['text_contact']  = 'Contact Us';
 $_['text_sitemap']  = 'Site Map';
+$_['text_customer_support'] = '1:1 Customer Support';
 ?>
---
+++ trunk/opencart149/catalog/language/turkish/account/customer_support.php
@@ -0,0 +1,44 @@
+<?php
+// Heading 
+$_['heading_title']      		= '1:1 Müşteri Hizmletleri';
+
+// Column
+$_['column_no']					= 'No';
+$_['column_subject']			= 'Konu';
+$_['column_created_at']			= 'Tarih';
+$_['column_answered']			= 'Cevaplandı Mı?';
+$_['column_answer']				= 'Cevap';
+$_['column_enquiry']			= 'İstek';
+$_['column_category_1st']		= '1. Kategori';
+$_['column_category_2nd']		= '2. Kategori';
+$_['column_action']				= 'Aksiyon';
+
+// Text
+$_['text_customer_support']   	= 'Müşteri Hizmetleri';
+$_['text_view']					= 'Görünüm';
+$_['text_no_answer']			= 'Cevaplanmadı';
+$_['text_answer_y']				= 'E';
+$_['text_answer_n']				= 'H';
+$_['text_answer_date']			= 'Cevaplandığı Tarih';
+$_['text_enquiry_date']			= 'Gönderildiği Tarih';
+$_['text_wait']					= 'Lütfen bekleyiniz';
+$_['text_success']				= 'Bildirim yaptığınız için teşekkürler. Bİldiriminiz cevaplaması için yöneticiye gönderildi.';
+$_['text_success_delete']		= 'Bildiriminiz Silindi..';
+$_['text_confirm_delete']		= 'Silmek istediğinize emin misiniz?';
+$_['text_no_results']			= 'Bir bildiriminiz bulunmamaktadır';
+
+
+// Button
+$_['button_enquiry']			= 'Yeni Bildirim';
+$_['button_delete']				= 'Sil';
+$_['button_submit']				= 'Gönder';
+
+// Errors
+$_['error_no_subject']			= 'Hata: Konu kısmı 3 ile 250 karakter arasında olmalıdır!';
+$_['error_no_enquiry']			= 'Hata: Mesaj kısmı 25 ile 250 karakter arasında olmalıdır!';
+$_['error_no_login']			= 'Hata: Lütfen sisteme giriş yapınız.';
+$_['error_no_enquiry_id']		= 'Hata: Mesaj alanı zorunludur.';
+$_['error_not_found_enquiry']	= 'Hata: Özür dileriz, Mesajınız mevcut değil.';
+$_['error_no_permission']		= 'Hata: Silem için yetkiniz bulunmamaktadır.';
+$_['error_already_answered']	= 'Hata: Bildiriminiz cevaplandığı için silinemez';
+?>

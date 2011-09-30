fcaab95d1d6be465b8f4a99c3f537e634a852529
ramazanfirin@gmail.com <ramazanfirin@gmail.com@3e63ede2-dacc-7fd9-6485-31b5a1e49b6f>
2011-09-30 20:49:09 +0000

    customer support

---
+++ opencart149/admin/language/english/catalog/customer_support.php
@@ -0,0 +1,38 @@
+<?php
+// Heading
+$_['heading_title']       	= '1:1 Customer Suport';
+
+// Text
+$_['text_success']      	= 'Success: You have modified customer support!';
+$_['text_not_answered']		= 'Not answered';
+
+// Column
+$_['column_no']    			= 'No';
+$_['column_store'] 			= 'Store';
+$_['column_1st_category']   	= '1st Category';
+$_['column_2nd_category']   	= '2nd Category';
+$_['column_customer']   	= 'Customer';
+$_['column_subject']    	= 'Subject';
+$_['column_enquiry']    	= 'Enquiry';
+$_['column_answer']     	= 'Answer';
+$_['column_date_added'] 	= 'Date Added';
+$_['column_date_answer'] 	= 'Date Answer';
+$_['column_action']     	= 'Action';
+
+// Entry
+$_['entry_no']    			= 'No:';
+$_['entry_store'] 			= 'Store:';
+$_['entry_1st_category']   	= '1st Category:';
+$_['entry_2nd_category']   	= '2nd Category:';
+$_['entry_customer']   		= 'Customer:';
+$_['entry_subject']    		= 'Subject:';
+$_['entry_enquiry']    		= 'Enquiry:';
+$_['entry_answer']    		= 'Answer:';
+
+// Button
+$_['button_delete']			= 'Delete';
+$_['button_manage_category']= 'Manage Category';
+// Error
+$_['error_permission']  	= 'Warning: You do not have permission to modify customer support!';
+$_['error_answer']  	= 'Error: Please enter your answer!';
+?>

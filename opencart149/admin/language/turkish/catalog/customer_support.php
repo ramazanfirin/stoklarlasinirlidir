c85c58e50281400717c413bc44a86f2dbb6d3f8f
ramazanfirin@gmail.com <ramazanfirin@gmail.com@3e63ede2-dacc-7fd9-6485-31b5a1e49b6f>
2011-09-30 20:52:21 +0000

    customer support

---
+++ opencart149/admin/language/turkish/catalog/customer_support.php
@@ -0,0 +1,38 @@
+<?php
+// Heading
+$_['heading_title']       	= '1:1 Müşteri Hizmetleri';
+
+// Text
+$_['text_success']      	= 'Success: You have modified customer support!';
+$_['text_not_answered']		= 'Cevap Verilmedi';
+
+// Column
+$_['column_no']    			= 'No';
+$_['column_store'] 			= 'Mağaza';
+$_['column_1st_category']   	= '1. Kategori';
+$_['column_2nd_category']   	= '2. Kategori';
+$_['column_customer']   	= 'Müşteri';
+$_['column_subject']    	= 'Konu';
+$_['column_enquiry']    	= 'İstek';
+$_['column_answer']     	= 'Cevap';
+$_['column_date_added'] 	= 'Eklenme Tarihi';
+$_['column_date_answer'] 	= 'Cevap Tarihi';
+$_['column_action']     	= 'Aksiyon';
+
+// Entry
+$_['entry_no']    			= 'No:';
+$_['entry_store'] 			= 'Mağaza:';
+$_['entry_1st_category']   	= '1. Kategori:';
+$_['entry_2nd_category']   	= '2. Kategori:';
+$_['entry_customer']   		= 'Müşteri:';
+$_['entry_subject']    		= 'Konu:';
+$_['entry_enquiry']    		= 'İstek:';
+$_['entry_answer']    		= 'Cevap:';
+
+// Button
+$_['button_delete']			= 'Sil';
+$_['button_manage_category']= 'Kategorileri Yönet';
+// Error
+$_['error_permission']  	= 'Uyarı: Bu işlem için yetkiniz yoktur!';
+$_['error_answer']  	= 'Hata: LÜtfen mesajınızı giriniz.!';
+?>

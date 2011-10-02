63fc84f22fea04980e54dbc83f31c189de2eeb2d
ramazanfirin@gmail.com <ramazanfirin@gmail.com@3e63ede2-dacc-7fd9-6485-31b5a1e49b6f>
2011-10-02 21:14:57 +0000

    tell a friend

---
+++ opencart149/catalog/language/turkish/information/tellafriend.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']     = 'Arkadaşlarınıza Tavsiye Ediniz';
+
+// Text
+$_['text_tell_friends'] = 'Arkadaş(lar)ınıza bizi tavsiye ediniz';
+$_['text_enter_friend'] = 'Arkadaş(lar)ınızın e-posta adresini giriniz:';
+$_['text_message']      = 'E-posta, adınızı ve e-posta adresinizi içerecek şekilde gönderilecektir.';
+$_['text_addresses']    = '<span class="required">*</span>Biz e-posta adresilerini kesinlikle saklamıyoruz.<span class="required">*</span>';
+$_['text_click']        = 'Bildiriyi Gönder';
+$_['text_thanks']       = '<h4>Arkadaş(lar)ınıza bizi tavsiye ettiğiniz için teşekkür ederiz.</h4>';
+$_['text_friend']       = 'Arkadaş(lar)ınıza tavsiye ediniz';
+
+// Entry
+$_['entry_name']        = 'Adınız:';
+$_['entry_email']       = 'E-Posta Adresiniz:';
+$_['entry_friend']      = 'Arkadaşınızın E-Posta Adresi:';
+
+// Button
+$_['button_add_friend'] = 'Arkadaş Ekle';
+$_['button_remove']     = 'Kaldır';
+
+// Error
+$_['error_name']        = 'İsim Gerekli!';
+$_['error_email']       = 'E-Posta adresi doğrulanamıyor!';
+$_['error_friend']      = 'En az bir arkadaş ekleyiniz';
+
+// Email
+$_['email_subject']     = 'A recommendation from %s';
+$_['email_greeting']    = 'Hi,';
+$_['email_text_1']      = 'A friend or colleague of yours, %s, whose email address is %s, thought you might like to visit our site.';
+$_['email_text_2']      = '%s has used our "Tell-a-Friend" form to send you this email.';
+$_['email_admin']       = 'A page has been sent';
+$_['email_sender']      = '%s has used the "Tell-a-Friend" form. Their email address is %s.';
+$_['email_recommend']   = 'The following people have been recommended to your site:';
+?>
